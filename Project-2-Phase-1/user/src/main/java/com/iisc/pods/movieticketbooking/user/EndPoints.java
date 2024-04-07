1e8ce893002650e269bac298bd6cb464f0a4ad7f
Suved G <vedghanmode@gmail.com>
2024-04-07 22:51:49 +0530

    Added docker host

--- Project-2-Phase-1/user/src/main/java/com/iisc/pods/movieticketbooking/user/EndPoints.java
+++ Project-2-Phase-1/user/src/main/java/com/iisc/pods/movieticketbooking/user/EndPoints.java
@@ -1,7 +1,7 @@
 package com.iisc.pods.movieticketbooking.user;
 
 public final class EndPoints {
-    private static final String BASE_URL = "http://localhost";
+    private static final String BASE_URL = "http://host.docker.internal";
     public static final String USER = BASE_URL + ":8080";
     public static final String BOOKING = BASE_URL + ":8081";
     public static final String WALLET = BASE_URL + ":8082";
